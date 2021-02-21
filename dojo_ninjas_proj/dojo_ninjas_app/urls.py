7a7ade7fd172666865a4afa13c00e33ccb3cc4a9
bhansen0091 <bhansen0091@yahoo.com>
2021-02-20 18:28:53 -0600

    assignment update

--- dojo_ninjas_proj/dojo_ninjas_app/urls.py
+++ dojo_ninjas_proj/dojo_ninjas_app/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('create_dojo', views.create_dojo),
+    path('create_ninja', views.create_ninja),
+    path('reset', views.reset),
 ]
--- dojo_ninjas_proj/dojo_ninjas_app/views.py
+++ dojo_ninjas_proj/dojo_ninjas_app/views.py
@@ -1,4 +1,30 @@
 from django.shortcuts import render, redirect
+from .models import Dojo, Ninja
 
 def index(request):
-    return render(request, "index.html")
+    context = {
+        "dojos": Dojo.objects.all(),
+        "ninjas": Ninja.objects.all(),
+    }
+    return render(request, "index.html", context)
+
+def create_dojo(request):
+    Dojo.objects.create(
+        name = request.POST['dojo_name'],
+        city = request.POST['dojo_city'],
+        state = request.POST['dojo_state'],
+        desc = "A brief description"
+    )
+    return redirect("/")
+
+def create_ninja(request):
+    Ninja.objects.create(
+        dojo = Dojo.objects.get(id = request.POST['dojo_select']),
+        first_name = request.POST['ninja_first_name'],
+        last_name = request.POST['ninja_last_name']
+    )
+    return redirect("/")
+
+def reset(request):
+    request.session.clear()
+    return redirect("/")
